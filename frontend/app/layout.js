6eadf4090f423ebb3b8ea0a4477dafb43080e9c8
Aaryan Rajpurohit <aaryanrpurohit@gmail.com>
2025-10-25 20:49:26 +0530

    Adding some packages

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,6 +4,7 @@ import {
   ClerkProvider
 } from '@clerk/nextjs'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
         <Analytics/>
+        <SpeedInsights/>
         {children}
       </body>
     </html>
